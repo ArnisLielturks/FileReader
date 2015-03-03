43b118d16aa67ec8721b535819050331ff772652
arnislielturks <arnis.lielturks@gmail.com>
2015-03-03 10:22:43 +0000

    makefile added

--- BinaryFileReader.cpp
+++ BinaryFileReader.cpp
@@ -19,10 +19,15 @@ void BinaryFileReader::ReadValues()
         //Go to the beggining of the file
         _file.seekg(0);
         char number;
+        int i = 0;
         while(_file.good() && !_file.eof()) {
             //Read char from the file
             if(_file.get(number))
-                cout << "number : " << (int)number << endl;
+                i++;
+                cout << "number : " << number << endl;
+                // if (i == setOutFile)
+                    // return;
+                saveOutFile(number);
         }
     } else {
         cout << "Error while openning file!" << endl;
@@ -38,5 +43,21 @@ void BinaryFileReader::SaveValues()
         }
     } else {
         cout << "Error while openning file" << endl;
+    }
+}
+
+void BinaryFileReader::setOutFile(const std::string filename)
+{
+    _ofile.open(filename.c_str(), ios::binary | ios::in | ios::out | ios::trunc);
+}
+
+void BinaryFileReader::saveOutFile(char info)
+{
+    if(_ofile.is_open()) {
+        int x = info;
+        if (x == 25) {
+            x = 100;
+        }
+        _ofile.put(x);
     }
 }
