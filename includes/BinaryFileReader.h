2df9abcb7a54721e45b86e58fca9dba58f997e28
arnislielturks <arnis.lielturks@gmail.com>
2015-03-09 12:36:34 +0000

    optimization + Awesome file type writer

--- includes/BinaryFileReader.h
+++ includes/BinaryFileReader.h
@@ -2,21 +2,33 @@
 #include <fstream>
 #include <cstring>
 #include <stdlib.h>
-#include <string>
+
 using namespace std;
 
+#define AWS_NAME_SIZE 60
+
+
+//Struct size should be dividable by 4
 struct AwesomeFileHeader {
     int ID;
-    char name[40];
+    char name[AWS_NAME_SIZE];
     float width;
     float height;
+    int verticeCount;
 };
+
+#define BYTE_AWS sizeof(AwesomeFileHeader)
+#define BYTE_AWS_NAME sizeof(char[AWS_NAME_SIZE])
+#define BYTE_INT 4
+#define BYTE_FLOAT 4
 
 struct AwesomeFileBody {
     float x;
     float y;
     float z;
 };
+
+#define BYTE_AWS_BODY sizeof(AwesomeFileBody)
 
 class BinaryFileReader
 {
@@ -30,4 +42,10 @@ public:
     void SaveValues();
 private:
     fstream _file;
+
+    char _readInt[4];
+    char _readFloat[4];
+
+    void storeInt(char intByte, int pos);
+    void storeFloat(char intByte, int pos);
 };
--- src/BinaryFileReader.cpp
+++ src/BinaryFileReader.cpp
@@ -19,46 +19,80 @@ void BinaryFileReader::ReadValues()
         //Go to the beggining of the file
         _file.seekg(0);
         int i = 0;
+        int k = 0;
         char single_char;
         char id[4];
-        char *title = new char[sizeof(AwesomeFileHeader().name)];
+        char *title = new char[AWS_NAME_SIZE];
         char size[4];
         char size2[4];
-        cout << "name sizeof " << sizeof(AwesomeFileHeader().name) << endl;
+        char vCount[4];
+
+        char temp[4];
+
+        cout << "name sizeof " << BYTE_AWS_NAME << endl;
         while(_file.good() && !_file.eof()) {
             //Read char from the file
             if(_file.get(single_char)) {
 
-                if (i < sizeof(int)) {
-                    id[i] = single_char;
-                    cout << "Reading int byte" << endl;
+                if (i < BYTE_INT) {
+                    // id[i] = single_char;
+                    storeInt(single_char, i);
+                    // cout << "Reading int byte" << endl;
 
-                } else if (i < sizeof(int) + sizeof(AwesomeFileHeader().name)) {
-                    title[i - sizeof(int)] = single_char;
-                    cout << "Reading string byte " << i - sizeof(int) << endl;
-
-                } else if (i < sizeof(int) + sizeof(AwesomeFileHeader().name) + sizeof(float)) {
-                    size[i - (sizeof(int) + sizeof(AwesomeFileHeader().name))] = single_char;
-                    cout << "Reading float byte 1" << endl;
-                } else if (i < sizeof(int) + sizeof(AwesomeFileHeader().name) + sizeof(float) * 2) {
-                    size2[i - (sizeof(int) + sizeof(AwesomeFileHeader().name) + sizeof(float))] = single_char;
-                    cout << "Reading float byte 2" << endl;
+                } else if (i < BYTE_INT + BYTE_AWS_NAME) {
+                    title[i - BYTE_INT] = single_char;
+                } else if (i < BYTE_INT + BYTE_AWS_NAME + BYTE_FLOAT) {
+                    storeFloat(single_char, i - (BYTE_INT + BYTE_AWS_NAME));
+                } else if (i < BYTE_INT + BYTE_AWS_NAME + BYTE_FLOAT * 2) {
+                    storeFloat(single_char, i - (BYTE_INT + BYTE_AWS_NAME + BYTE_FLOAT));
+                } else if (i < BYTE_INT + BYTE_AWS_NAME + BYTE_FLOAT * 2 + BYTE_INT) {
+                    storeInt(single_char, i - (BYTE_INT + BYTE_AWS_NAME + BYTE_FLOAT * 2));
+                } else {
+                    // cout << "K " << k << endl;
+                    // temp[k] = single_char;
+                    storeFloat(single_char, k);
+                    k++;
+                    if (k > 3) {
+                        k = 0;
+                    //     float *t = (float*)temp;
+                    //     cout << "Reading vector value " << *t << endl;
+                    }
                 }
                 i++;
             }
         }
 
         //Convert from char array to int/float
-        int *result = (int*)id;
-        float *result2 = (float*)size;
-        float *result3 = (float*)size2;
+  //       int *result = (int*)id;
+  //       float *result2 = (float*)size;
+  //       float *result3 = (float*)size2;
 		string obj_name = string(title);
-        cout << "ID         :" << *result << endl;
+  //       int *vertCount = (int*)vCount;
+  //       cout << "ID         :" << *result << endl;
         cout << "Name       :" << obj_name << endl;
-        cout << "width      :" << *result2 << endl;
-        cout << "height     :" << *result3 << endl;
+  //       cout << "width      :" << *result2 << endl;
+  //       cout << "height     :" << *result3 << endl;
+  //       cout << "vertice count : " << *vertCount << endl;
     } else {
         cout << "Error while openning file!" << endl;
+    }
+}
+
+void BinaryFileReader::storeInt(char intByte, int pos)
+{
+    _readInt[pos] = intByte;
+    if (pos == 3) {
+        int *t = (int*)_readInt;
+        cout << "Int ready --- " << *t << endl;
+    }
+}
+
+void BinaryFileReader::storeFloat(char intByte, int pos)
+{
+    _readFloat[pos] = intByte;
+    if (pos == 3) {
+        float *t = (float*)_readFloat;
+        cout << "Float ready --- " << *t << endl;
     }
 }
 
@@ -70,25 +104,45 @@ void BinaryFileReader::SaveValues()
         //Add some data to the struct
         header.ID = 1991;
 
-        string name = "Oops!";
-        for(int i = 0; i < sizeof(AwesomeFileHeader().name) - 1; i++) {
-			if (i < name.size()) 
+        string name = "Oops! I didnt want this to happen Im really sorry. HA!";
+        for(int i = 0; i < BYTE_AWS_NAME - 1; i++) {
+			if (i < name.size() - 1) {
 				header.name[i] = name[i];
+            } else {
+                header.name[i] = '\0';
+            }
         }
 
         //Add the string ending char
-        header.name[sizeof(AwesomeFileHeader().name) - 1] = '\0';
+        header.name[BYTE_AWS_NAME - 1] = '\0';
 
         header.width = 222.2f;
         header.height = 333.3f;
-        char *data = new char[sizeof(AwesomeFileHeader)];
-        memcpy(data, &header, sizeof(AwesomeFileHeader));
-        cout << "File size " << sizeof(AwesomeFileHeader) << endl;
 
-        for(int i = 0; i < sizeof(AwesomeFileHeader); i++) {
+        header.verticeCount = 10;
+        char *data = new char[BYTE_AWS];
+        memcpy(data, &header, BYTE_AWS);
+        cout << "File size " << BYTE_AWS << endl;
+
+        for(int i = 0; i < BYTE_AWS; i++) {
             _file.put(data[i]);
-            cout << data[i] << endl;
+            // cout << data[i] << endl;
         }
+
+        //Append body
+        AwesomeFileBody tempBody;
+        tempBody.x = 3.1;
+        tempBody.y = 4.1;
+        tempBody.z = 5.1;
+        char *body = new char[BYTE_AWS_BODY];
+        memcpy(body, &tempBody, BYTE_AWS_BODY);
+        for (int k = 0; k < 4; k++) {
+            for(int i = 0; i < BYTE_AWS_BODY; i++) {
+                _file.put(body[i]);
+                // cout << body[i] << endl;
+            }
+        }
+
     } else {
         cout << "Error while openning file" << endl;
     }
